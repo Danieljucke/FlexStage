631e0c2c600fe5cb0085ce51c666101440e9f2f1
LeonardoRubuz <rubuzleonardo@gmail.com>
2022-09-19 12:03:22 +0100

    Modification du formulaire d'ajout d'utilisateur

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     #[Route('/default', name: 'app_default')]
-    public function index(): Response
+    public function index(Request $request, ManagerRegistry $doctrine): Response
     {
+
         return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+            'form' => $form->createView(),
         ]);
     }
 }
