12828252a106508fc8714ca67ea249e94b755b72
Joy Daniel <88509670+Danieljucke@users.noreply.github.com>
2022-09-21 13:02:52 +0100

    ajouts entités

---
+++ src/Entity/Paiement.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PaiementRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PaiementRepository::class)]
+class Paiement
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?float $montant = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $mode_paiement = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date_paiement = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMontant(): ?float
+    {
+        return $this->montant;
+    }
+
+    public function setMontant(float $montant): self
+    {
+        $this->montant = $montant;
+
+        return $this;
+    }
+
+    public function getModePaiement(): ?string
+    {
+        return $this->mode_paiement;
+    }
+
+    public function setModePaiement(string $mode_paiement): self
+    {
+        $this->mode_paiement = $mode_paiement;
+
+        return $this;
+    }
+
+    public function getDatePaiement(): ?\DateTimeInterface
+    {
+        return $this->date_paiement;
+    }
+
+    public function setDatePaiement(\DateTimeInterface $date_paiement): self
+    {
+        $this->date_paiement = $date_paiement;
+
+        return $this;
+    }
+}
