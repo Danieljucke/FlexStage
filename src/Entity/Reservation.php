a9d9b206f75b7e09bc337910372b0c42c24c72da
Joy Daniel <88509670+Danieljucke@users.noreply.github.com>
2022-09-21 15:25:05 +0100

    upload de l'entité users

--- src/Entity/Reservation.php
+++ src/Entity/Reservation.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\ReservationRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ReservationRepository::class)]
@@ -17,6 +18,22 @@ class Reservation
 
     #[ORM\ManyToMany(targetEntity: Service::class, inversedBy: 'reservations')]
     private Collection $service;
+
+    #[ORM\ManyToOne(inversedBy: 'reservation')]
+    private ?Paiement $paiement = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $date_arriver = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $date_fin = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $motif = null;
+
+    #[ORM\ManyToOne(inversedBy: 'reservations')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Users $user = null;
 
     public function __construct()
     {
@@ -48,6 +65,66 @@ class Reservation
     public function removeService(Service $service): self
     {
         $this->service->removeElement($service);
+
+        return $this;
+    }
+
+    public function getPaiement(): ?Paiement
+    {
+        return $this->paiement;
+    }
+
+    public function setPaiement(?Paiement $paiement): self
+    {
+        $this->paiement = $paiement;
+
+        return $this;
+    }
+
+    public function getDateArriver(): ?\DateTimeInterface
+    {
+        return $this->date_arriver;
+    }
+
+    public function setDateArriver(?\DateTimeInterface $date_arriver): self
+    {
+        $this->date_arriver = $date_arriver;
+
+        return $this;
+    }
+
+    public function getDateFin(): ?\DateTimeInterface
+    {
+        return $this->date_fin;
+    }
+
+    public function setDateFin(?\DateTimeInterface $date_fin): self
+    {
+        $this->date_fin = $date_fin;
+
+        return $this;
+    }
+
+    public function getMotif(): ?string
+    {
+        return $this->motif;
+    }
+
+    public function setMotif(string $motif): self
+    {
+        $this->motif = $motif;
+
+        return $this;
+    }
+
+    public function getUser(): ?Users
+    {
+        return $this->user;
+    }
+
+    public function setUser(?Users $user): self
+    {
+        $this->user = $user;
 
         return $this;
     }
