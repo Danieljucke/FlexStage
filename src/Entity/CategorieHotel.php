b9dbb49f0049573311a0287ced80f7cdaa3c366d
LeonardoRubuz <rubuzleonardo@gmail.com>
2022-09-23 16:12:23 +0100

    Formulaire d'ajout d'hotel presque terminé Reste: Ajout des validateurs

--- src/Entity/CategorieHotel.php
+++ src/Entity/CategorieHotel.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CategorieHotelRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CategorieHotelRepository::class)]
@@ -15,6 +17,14 @@ class CategorieHotel
 
     #[ORM\Column(length: 255)]
     private ?string $denomination = null;
+
+    #[ORM\ManyToMany(targetEntity: Hotel::class, mappedBy: 'categorie')]
+    private Collection $hotels;
+
+    public function __construct()
+    {
+        $this->hotels = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -34,5 +44,32 @@ class CategorieHotel
     }
     public function __toString() {
         return $this->denomination;
+    }
+
+    /**
+     * @return Collection<int, Hotel>
+     */
+    public function getHotels(): Collection
+    {
+        return $this->hotels;
+    }
+
+    public function addHotel(Hotel $hotel): self
+    {
+        if (!$this->hotels->contains($hotel)) {
+            $this->hotels->add($hotel);
+            $hotel->addCategorie($this);
+        }
+
+        return $this;
+    }
+
+    public function removeHotel(Hotel $hotel): self
+    {
+        if ($this->hotels->removeElement($hotel)) {
+            $hotel->removeCategorie($this);
+        }
+
+        return $this;
     }
 }
